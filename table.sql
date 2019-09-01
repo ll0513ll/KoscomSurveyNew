9469ab005110765f9260fa8f2d7a1a6af40a952b
ll0513ll <sungmo58486@gmail.com>
2019-09-01 19:40:33 +0900

    DB정보 추가

--- table.sql
+++ table.sql
@@ -1,47 +1,9 @@
-CREATE TABLE `survey` (
-  `surveyNo` int(11) NOT NULL AUTO_INCREMENT,
-  `quesNo` int(11) DEFAULT NULL,
-  `cateNo` int(11) DEFAULT NULL,
-  `companyNo` int(11) DEFAULT NULL,
-  `manager` varchar(30) DEFAULT NULL,
-  `surveyDate` date DEFAULT NULL,
-  `type` int(10) DEFAULT NULL,
-  `answer` varchar(500) DEFAULT NULL,
-  `dissatisReason` varchar(500) DEFAULT NULL,
-  `satisVal` int(10) DEFAULT NULL,
-  PRIMARY KEY (`surveyNo`),
-  KEY `quesNo` (`quesNo`),
-  KEY `cateNo` (`cateNo`),
-  KEY `companyNo` (`companyNo`),
-  CONSTRAINT `survey_ibfk_1` FOREIGN KEY (`quesNo`) REFERENCES `question` (`quesNo`),
-  CONSTRAINT `survey_ibfk_2` FOREIGN KEY (`cateNo`) REFERENCES `cate` (`cateNo`),
-  CONSTRAINT `survey_ibfk_3` FOREIGN KEY (`companyNo`) REFERENCES `company` (`companyNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8
-
-
-CREATE TABLE `surveyForm` (
-                          `surveyFormGroupNo` int(11) NOT NULL AUTO_INCREMENT,
-                          `cateName` varchar(100) NOT NULL,
-                          `regDate` date DEFAULT NULL,
-                          PRIMARY KEY (`surveyFormGroupNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
-
-CREATE TABLE `surveyForm` (
-                          `surveyFormNo` int(11) NOT NULL AUTO_INCREMENT,
-                          `surveyFormGroupNo` int(10) NOT NULL,
-                          `quesName` varchar(100) DEFAULT NULL,
-                          `type` int(10) DEFAULT NULL,
-                          `order` int(10) DEFAULT NULL,
-                          PRIMARY KEY (`surveyFormNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
-
 CREATE TABLE `cate` (
   `cateNo` int(11) NOT NULL AUTO_INCREMENT,
   `cateName` varchar(100) NOT NULL,
   `cateInfo` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`cateNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8
 
 
 CREATE TABLE `company` (
@@ -50,7 +12,6 @@ CREATE TABLE `company` (
   `manager` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`companyNo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8
-
 
 CREATE TABLE `question` (
   `quesNo` int(11) NOT NULL AUTO_INCREMENT,
@@ -63,8 +24,49 @@ CREATE TABLE `question` (
   KEY `companyNo` (`companyNo`),
   CONSTRAINT `question_ibfk_1` FOREIGN KEY (`cateNo`) REFERENCES `cate` (`cateNo`),
   CONSTRAINT `question_ibfk_2` FOREIGN KEY (`companyNo`) REFERENCES `company` (`companyNo`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8
 
+
+CREATE TABLE `survey` (
+  `surveyGroupNo` int(11) NOT NULL,
+  `surveyNo` int(11) NOT NULL AUTO_INCREMENT,
+  `companyNo` int(11) DEFAULT NULL,
+  `quesName` varchar(200) DEFAULT NULL,
+  `cateName` varchar(200) DEFAULT NULL,
+  `manager` varchar(30) DEFAULT NULL COMMENT '담당자',
+  `surveyDate` date DEFAULT NULL COMMENT '설문지 생성일',
+  `type` int(10) DEFAULT NULL COMMENT '객관식/주관식 타입',
+  `answer` varchar(500) DEFAULT NULL COMMENT '주관식 답변',
+  `dissatisReason` varchar(500) DEFAULT NULL COMMENT '객관식 나쁨 선택 사유',
+  `satisVal` int(10) DEFAULT NULL COMMENT '객관식 만족도 선택',
+  `participate` int(10) DEFAULT '0',
+  PRIMARY KEY (`surveyNo`),
+  KEY `companyNo` (`companyNo`),
+  CONSTRAINT `survey_ibfk_3` FOREIGN KEY (`companyNo`) REFERENCES `company` (`companyNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=403 DEFAULT CHARSET=utf8
+
+
+CREATE TABLE `surveyform` (
+  `surveyFormNo` int(11) NOT NULL AUTO_INCREMENT,
+  `surveyFormGroupNo` int(11) NOT NULL,
+  `quesName` varchar(100) DEFAULT NULL,
+  `type` int(10) DEFAULT NULL,
+  `orderNo` int(10) DEFAULT NULL,
+  `quesNo` int(11) DEFAULT NULL,
+  PRIMARY KEY (`surveyFormNo`),
+  KEY `quesNo` (`quesNo`),
+  CONSTRAINT `quesNo` FOREIGN KEY (`quesNo`) REFERENCES `question` (`quesNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=420 DEFAULT CHARSET=utf8
+
+
+CREATE TABLE `surveyformgroup` (
+  `endDate` date DEFAULT NULL,
+  `startDate` date DEFAULT NULL,
+  `surveyFormGroupNo` int(11) NOT NULL AUTO_INCREMENT,
+  `cateName` varchar(100) NOT NULL,
+  `regDate` date DEFAULT NULL,
+  PRIMARY KEY (`surveyFormGroupNo`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=utf8
 
 CREATE TABLE `user` (
   `userNo` int(11) NOT NULL AUTO_INCREMENT,
@@ -75,4 +77,3 @@ CREATE TABLE `user` (
   `telephone` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`userNo`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
-
